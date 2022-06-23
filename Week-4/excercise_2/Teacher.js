6a042e540f7c5c88602cb7bc81d5a391ef3fc6c0
yogesh <er.yogeshkataria08@gmail.com>
2022-06-23 13:29:02 +0530

    week -4

---
+++ Week-4/excercise_2/Teacher.js
@@ -0,0 +1,21 @@
+// Inheritance Example
+
+var Person = function () {};
+
+Person.prototype.initialize = function (name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// create the class Teacher and a method teach
+var Teacher = function () {
+    this.teach = function (subject) {
+        console.log(this.name + " is teaching " + subject);
+    }
+}
+Teacher.prototype = new Person();
+
+var him = new Teacher();
+
+him.initialize("Adam", 45);
+him.teach("Inheritance");
